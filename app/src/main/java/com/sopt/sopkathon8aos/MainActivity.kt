524184768ddf5f5726f80e05571010832f61380d
이빵주 <yungju9982@naver.com>
2022-11-19 13:51:44 +0900

    #1 [add] 프로젝트 init

---
+++ app/src/main/java/com/sopt/sopkathon8aos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sopt.sopkathon8aos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
