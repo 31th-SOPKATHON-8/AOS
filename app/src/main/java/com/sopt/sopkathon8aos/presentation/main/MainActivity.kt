d8a51897ff49a43986d2d188465432b513558cc3
이빵주 <yungju9982@naver.com>
2022-11-19 14:10:19 +0900

    #1 [add] app단 기초세팅

---
+++ app/src/main/java/com/sopt/sopkathon8aos/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.sopkathon8aos.presentation.main
+
+import android.os.Bundle
+import com.sopt.sopkathon8aos.R
+import com.sopt.sopkathon8aos.databinding.ActivityMainBinding
+import com.sopt.sopkathon8aos.util.binding.BindingActivity
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
