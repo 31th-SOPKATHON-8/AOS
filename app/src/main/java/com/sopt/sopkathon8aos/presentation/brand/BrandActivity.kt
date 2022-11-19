66a94441ad56ead9b4358bf15200df19bc56bc7f
이빵주 <yungju9982@naver.com>
2022-11-20 07:29:54 +0900

    #10 [feat] Brand 서버 구현

--- app/src/main/java/com/sopt/sopkathon8aos/presentation/brand/BrandActivity.kt
+++ app/src/main/java/com/sopt/sopkathon8aos/presentation/brand/BrandActivity.kt
@@ -1,12 +1,18 @@
 package com.sopt.sopkathon8aos.presentation.brand
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.sopt.sopkathon8aos.R
 import com.sopt.sopkathon8aos.databinding.ActivityBrandBinding
 import com.sopt.sopkathon8aos.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BrandActivity : BindingActivity<ActivityBrandBinding>(R.layout.activity_brand) {
+    private val brandViewModel: BrandViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.vm = brandViewModel
+        brandViewModel.getBrand(0)
     }
 }
