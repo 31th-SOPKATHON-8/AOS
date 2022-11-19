dc8ea4cd8737a64a69d3ac5f2760d8cf7b9ca0ef
IslandofDream <rhffldkt0517@naver.com>
2022-11-19 18:56:54 +0900

    [Chore] Add OnSingleClickListener

--- app/src/main/java/com/sopt/sopkathon8aos/util/extensions/ContextExt.kt
+++ app/src/main/java/com/sopt/sopkathon8aos/util/extensions/ContextExt.kt
@@ -1,8 +1,13 @@
 package com.sopt.sopkathon8aos.util.extensions
 
 import android.content.Context
+import android.view.View
 import android.widget.Toast
 
 fun Context.showToast(msg: String) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+fun View.setOnSingleClickListener(onSingleClick: (View) -> Unit) {
+    setOnClickListener(OnSingleClickListener { onSingleClick(it) })
 }
