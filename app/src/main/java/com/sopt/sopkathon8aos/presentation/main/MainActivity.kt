7e7b6dfb8432d168b65ebb2c7c64cd3f98dfbeaa
이빵주 <yungju9982@naver.com>
2022-11-19 15:21:49 +0900

    #1 [add] application 생성

--- app/src/main/java/com/sopt/sopkathon8aos/presentation/main/MainActivity.kt
+++ app/src/main/java/com/sopt/sopkathon8aos/presentation/main/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import com.sopt.sopkathon8aos.R
 import com.sopt.sopkathon8aos.databinding.ActivityMainBinding
 import com.sopt.sopkathon8aos.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
